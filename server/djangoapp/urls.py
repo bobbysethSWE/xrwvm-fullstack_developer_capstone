4eb18d855de3b5add6407d57881103ed6f80697a
Arham <twinm148@gmail.com>
2025-03-03 11:30:58 -0500

    finished module 3

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -2,10 +2,20 @@
 # from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
+from django.urls import path
+from django.views.generic import TemplateView
+from django.contrib import admin
+from django.urls import path, include
+from django.urls import path
+from . import views  # Import your views module
 
-app_name = 'djangoapp'
+app_name = 'djangoapp' 
 urlpatterns = [
+    
+
+    # path('admin/', admin.site.urls),
+    # path('djangoapp/', include('djangoapp.urls')),
+    path('about/', TemplateView.as_view(template_name="About.html")),
     # # path for registration
 
     # path for login
